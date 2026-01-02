5200ef42a3ce117c93326c3d4da66af911d82f0d
Andrei Rudenko <rudenkopaint@gmail.com>
2026-01-03 01:33:09 +0200

    Change constructor to type.constructor for new expression

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -112,7 +112,7 @@
 (function_declaration name: "new" @constructor)
 (call_expression
   "new" @keyword
-  constructor: (type_name) @constructor
+  constructor: (type_name) @type.constructor
 )
 
 ; Tokens
